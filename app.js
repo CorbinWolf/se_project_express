050f797f26eb87604618ea74596eebfd8bef07cb
Corbin Wolf <corbinwolf300@gmail.com>
2025-07-09 08:41:39 -0700

    Refactor: fix endpoints error 2

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const mainRouter = require("./routes/index");
 
 const app = express();
 const { PORT = 3001 } = process.env;
-const _id = req.user;
+
+req.user = {
+  _id: "5d8b8592978f8bd833ca8133",
+};
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
