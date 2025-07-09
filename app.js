f42c0272c07567f035f5565cb0dea2ec84135c0f
Corbin Wolf <corbinwolf300@gmail.com>
2025-07-09 08:51:10 -0700

    Refactor: fix endpoints error 4

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const auth = require("./middlewares/auth");
 const mainRouter = require("./routes/index");
 
 const app = express();
@@ -14,7 +13,6 @@ mongoose
   .catch(console.error);
 
 app.use(express.json());
-app.use(auth);
 app.use("/", mainRouter);
 
 app.listen(PORT, () => {
--- middlewares/auth.js
+++
@@ -1,8 +0,0 @@
-const auth = (req, res, next) => {
-  req.user = {
-    _id: "66bd8591984fda4687c097c5",
-  };
-  next();
-};
-
-module.exports = auth;
